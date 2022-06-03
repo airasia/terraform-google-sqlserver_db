5f3f4506eda815e4bdb037a52daa6c2765fc4c90
Syed Rakib Al Hasan <975768+syedrakib@users.noreply.github.com>
2022-06-03 11:53:16 +0800

    Version bump google's sql-db module -> v4.5.0

--- main.tf
+++ main.tf
@@ -30,7 +30,7 @@ resource "google_project_service" "cloudsql_api" {
 
 module "google_sqlserver_db" {
   source                          = "GoogleCloudPlatform/sql-db/google//modules/mssql"
-  version                         = "4.3.0"
+  version                         = "4.5.0"
   depends_on                      = [google_project_service.compute_api, google_project_service.cloudsql_api]
   deletion_protection             = var.deletion_protection
   project_id                      = data.google_client_config.google_client.project
